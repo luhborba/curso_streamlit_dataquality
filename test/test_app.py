bf3fbac9d6d2b0755089d18bbd3c9c3a2f8794aa
LucianoBorba <luhborbafilho@gmail.com>
2024-03-28 21:23:16 -0300

    test: adicionando teste de aplicação

---
+++ test/test_app.py
@@ -0,0 +1,28 @@
+import subprocess
+
+import pytest
+from selenium import webdriver
+from time import sleep
+
+
+@pytest.fixture
+def driver():
+    processo = subprocess.Popen(["streamlit", "run", "main.py"])
+
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:8501")
+    driver.set_page_load_timeout(10)
+
+    yield driver
+
+    driver.quit()
+    processo.kill()
+
+def test_open_app(driver):
+    driver.get("http://localhost:8501")
+    sleep(2)
+
+def test_title_app(driver):
+    driver.get("http://localhost:8501")
+    sleep(2)
+    assert driver.title == "Validador de CSV"
