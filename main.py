9abfa95705a069edfc59e2e37cc6577644f09dc7
LucianoBorba <luhborbafilho@gmail.com>
2024-04-06 12:35:46 -0300

    feat: alterando função de validar para enviar dados para o bd

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pandas as pd
 import streamlit as st
+from datetime import datetime
 
 from src.schema import ContratoFuncionarios
+from src.database import criar_sessao, Funcionarios
 
 
-def validar(csv):
+def validar_e_inserir_no_banco(csv, session):
     """
-    Validar o csv.
+    Função de Validar o arquivo em formato csv e inserir dados no Banco de Dados.
 
     Args:
         csv (str): Caminho do arquivo csv.
+        session (Session): Sessão do SQLAlchemy.
     """
     try:
         df = pd.read_csv(csv)
         erros = []
+        dados_validos = []
 
         for idx, row in df.iterrows():
             try:
                 ContratoFuncionarios(**row.to_dict())
+                row['datanascimento'] = datetime.strptime(row['datanascimento'], '%Y-%m-%d')
+                dados_validos.append(Funcionarios(**row.to_dict()))
             except Exception as e:
                 erros.append(f"Error na Linha:{idx+2} de {e}")
 
@@ -26,7 +32,9 @@ def validar(csv):
             for erro in erros:
                 st.error(erro)
         else:
-            st.success("Arquivo Valido!")
+            session.add_all(dados_validos)
+            session.commit()
+            st.success("Arquivo Valido e Dados Inseridos no Banco de Dados!")
             return True
 
     except Exception as e:
@@ -37,13 +45,14 @@ def main():
     """Função principal."""
     st.set_page_config(page_title="Validador de CSV", layout="wide")
     st.title("Validador de CSV")
+    session = criar_sessao()
 
     csv = st.file_uploader("Escolha um arquivo CSV para validar", type=["csv"])
 
     botao = st.button("Validar")
 
     if botao:
-        validar(csv)
+        validar_e_inserir_no_banco(csv, session)
 
 
 if __name__ == "__main__":
